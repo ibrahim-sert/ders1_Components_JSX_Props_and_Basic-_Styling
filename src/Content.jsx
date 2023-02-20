ab101b47d98b2cf3e8a3643962860af5789b3099
ibrahim-sert <evo3506@gmail.com>
2023-02-20 17:33:27 +0300

    styling learning

--- src/Content.jsx
+++ src/Content.jsx
@@ -12,10 +12,40 @@
 // ? Dahili bir resim eklemek icin import yapmak gereklidir.
 // ? ancak public klasöründeki resimler import suz bir şekilde
 //? erişilebilir.
+import magnolia from "./img/magnolia.jpg"
+
+//?Harici stillendirme dosyası
+import './Content.css';
 
 const Content =()=>{
+  //?  Local Style Object
+  const pStyle={
+    fontFamily:"Tahoma",
+    fontSize:"1.3rem",
+    lineHeight:"1.5rem"
+  }
+
+  const imgStyle={
+    margin:"1rem auto",
+    display:"block",
+    width:"400px"
+  }
   return(
-    <div>Content</div>
+    <div>
+      {/* JSX */}
+      {/* Inline Styling */}
+      <h2 style={{color:"white",backgroundColor:"red"}}>React JS</h2>
+
+      {/* Local or global variables */}
+      <p style={pStyle}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, ut.</p>
+
+
+      <img style={imgStyle} src={magnolia} alt="spring-img1" />
+
+      <p className="par">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos, dolor pariatur nam quibusdam blanditiis.</p>
+
+      <p className="par">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rerum animi aperiam molestias saepe laudantium a, esse sed asperiores neque consequuntur provident, laborum aspernatur assumenda iusto voluptatum culpa ipsa velit?</p>
+    </div>
   )
 }
 
